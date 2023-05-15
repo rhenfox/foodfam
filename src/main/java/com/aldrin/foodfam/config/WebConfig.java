71847ad9452c76fce6b9d9b3e60469dac81153c4
aldri <aldri@ALDRIN>
2023-05-15 20:27:20 +0800

    First Commit

--- src/main/java/com/aldrin/foodfam/config/WebConfig.java
+++ src/main/java/com/aldrin/foodfam/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-          .addResourceHandler("/webjars/**","/templates/**","/static/**")
-          .addResourceLocations("/webjars/","/templates/","/static/");
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
     }
 }
